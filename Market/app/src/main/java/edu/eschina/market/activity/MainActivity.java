4b7d645c6dfc95d983b3eb15db5a52bca5c33cc1
taibai1024 <taibai2048@gmail.com>
2022-12-20 15:15:07 +0800

    优化include布局viewBinding

--- Market/app/src/main/java/edu/eschina/market/activity/MainActivity.java
+++ Market/app/src/main/java/edu/eschina/market/activity/MainActivity.java
@@ -1,11 +1,8 @@
 package edu.eschina.market.activity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.view.LayoutInflater;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import java.util.ArrayList;
@@ -13,36 +10,20 @@ import java.util.List;
 import edu.eschina.market.R;
 import edu.eschina.market.adapter.BottomNavigateAdapter;
 import edu.eschina.market.databinding.ActivityMainBinding;
-import edu.eschina.market.databinding.BottomNavLayoutBinding;
 import edu.eschina.market.fragment.HomeFragment;
 import edu.eschina.market.fragment.MeFragment;
 import edu.eschina.market.fragment.OrderFragment;
+
 public class MainActivity extends BaseViewModelActivity<ActivityMainBinding> {
 
     private SharedPreferences userSharedPreferences;
-    private LinearLayout navHome;
-    private LinearLayout navOrder;
-    private LinearLayout navMe;
-    private TextView tvHome;
-    private TextView tvOrder;
-    private TextView tvMe;
-    private ImageView ivHome;
-    private ImageView ivOrder;
-    private ImageView ivMe;
+    private edu.eschina.market.databinding.BottomNavLayoutBinding bottomNavLayoutBinding;
 
     @SuppressLint("CutPasteId")
     @Override
     protected void initViews() {
         super.initViews();
-        navHome = findViewById(R.id.nav_home);
-        navOrder = findViewById(R.id.nav_order);
-        navMe = findViewById(R.id.nav_me);
-        tvHome = findViewById(R.id.tv_home);
-        tvOrder = findViewById(R.id.tv_order);
-        tvMe = findViewById(R.id.tv_me);
-        ivHome = findViewById(R.id.iv_home);
-        ivOrder = findViewById(R.id.iv_order);
-        ivMe = findViewById(R.id.iv_me);
+        bottomNavLayoutBinding = viewBinding.bottomNav;
     }
 
     @Override
@@ -68,9 +49,10 @@ public class MainActivity extends BaseViewModelActivity<ActivityMainBinding> {
     @Override
     protected void initEvents() {
         super.initEvents();
-        navHome.setOnClickListener((view) -> viewBinding.viewpager.setCurrentItem(0));
-        navOrder.setOnClickListener((view) -> viewBinding.viewpager.setCurrentItem(1));
-        navMe.setOnClickListener((view) -> viewBinding.viewpager.setCurrentItem(2));
+
+        bottomNavLayoutBinding.navHome.setOnClickListener((view) -> viewBinding.viewpager.setCurrentItem(0));
+        bottomNavLayoutBinding.navOrder.setOnClickListener((view) -> viewBinding.viewpager.setCurrentItem(1));
+        bottomNavLayoutBinding.navMe.setOnClickListener((view) -> viewBinding.viewpager.setCurrentItem(2));
 
         //viewPageChange
         viewBinding.viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -80,7 +62,7 @@ public class MainActivity extends BaseViewModelActivity<ActivityMainBinding> {
             }
 
             @Override
-            public void onPageSelected(int position)     {
+            public void onPageSelected(int position) {
                 pageSelected(position);
             }
 
@@ -93,29 +75,29 @@ public class MainActivity extends BaseViewModelActivity<ActivityMainBinding> {
     }
 
     private void resetColor() {
-        ivHome.setSelected(false);
-        ivOrder.setSelected(false);
-        ivMe.setSelected(false);
-        tvHome.setTextColor(getResources().getColor(R.color.black));
-        tvOrder.setTextColor(getResources().getColor(R.color.black));
-        tvMe.setTextColor(getResources().getColor(R.color.black));
+        bottomNavLayoutBinding.ivHome.setSelected(false);
+        bottomNavLayoutBinding.ivOrder.setSelected(false);
+        bottomNavLayoutBinding.ivMe.setSelected(false);
+        bottomNavLayoutBinding.tvHome.setTextColor(getResources().getColor(R.color.black));
+        bottomNavLayoutBinding.tvOrder.setTextColor(getResources().getColor(R.color.black));
+        bottomNavLayoutBinding.tvMe.setTextColor(getResources().getColor(R.color.black));
     }
 
     private void pageSelected(int position) {
         resetColor();
         @SuppressLint("ResourceAsColor") Runnable[] runnable = {
                 () -> {
-            ivHome.setSelected(true);
-            tvHome.setTextColor(getResources().getColor(R.color.colorPrimary));
-        },
+                    bottomNavLayoutBinding.ivHome.setSelected(true);
+                    bottomNavLayoutBinding.tvHome.setTextColor(getResources().getColor(R.color.colorPrimary));
+                },
                 () -> {
-                    ivOrder.setSelected(true);
-                    tvOrder.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    bottomNavLayoutBinding.ivOrder.setSelected(true);
+                    bottomNavLayoutBinding.tvOrder.setTextColor(getResources().getColor(R.color.colorPrimary));
                 },
                 () ->
                 {
-                    ivMe.setSelected(true);
-                    tvMe.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    bottomNavLayoutBinding.ivMe.setSelected(true);
+                    bottomNavLayoutBinding.tvMe.setTextColor(getResources().getColor(R.color.colorPrimary));
                 },
         };
         runnable[position].run();
